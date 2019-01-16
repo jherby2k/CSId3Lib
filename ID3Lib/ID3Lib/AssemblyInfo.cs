fdae2fa2ae14a9b075516ab15fcb95277904e8a1
Jeremy Herbison <jeremy.herbison@gmail.com>
2019-01-16 14:33:20 -0500

    Strong-name sign the assembly.

--- ID3Lib/ID3Lib/AssemblyInfo.cs
+++ ID3Lib/ID3Lib/AssemblyInfo.cs
@@ -6,4 +6,9 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Id3Lib.Tests")]
+[assembly: InternalsVisibleTo("Id3Lib.Tests, PublicKey=" +
+"002400000480000094000000060200000024000052534131000400000100010085d2a47761934c" +
+"d1d11dce9cd8ac0ff5d5599ea2455f04ad49792f6d5d14a7c066c602493db1cfd21f3f3ca9302a" +
+"c78fdd310da8a1e6fe83cd2e4ce3c2a86d339207312e82d6ed1975a7b76df8334c1c5b3498e6b0" +
+"8d697e6fe2911590ea02ca62761c9bfe2c499643c419c4c81fe83da24b85861f461c2717e9c9c2" +
+"ba80abcd")]
